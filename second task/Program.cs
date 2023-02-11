b585d5fdc65534c85b56cb7381f1bc391f8b3053
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-11 18:42:42 +0300

    second task solved

---
+++ second task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая принимает на 
+//вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Input three numbers and i'll find a bigger number of them \nEnter first number:");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter second number:");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter third number:");
+double thirdNumber = Convert.ToDouble(Console.ReadLine());
+
+double max = firstNumber;
+
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+System.Console.WriteLine($"{max} is the biggest value!");
