005377b26764ed346dd2fbe90cf5e215db061a16
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-11 18:49:25 +0300

    third task solved

---
+++ third task/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число
+//и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("Hello there! Give me some integer number and i'll show you is the number even or not. \nEnter number:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("Your number is even.");
+}
+else
+{
+    System.Console.WriteLine("Your number is not even.");
+}
