ff09488f4302a7e3fdfd75815aada3e30eaf924b
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-11 18:33:35 +0300

    first task solved

---
+++ first task/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1: Напишите программу, 
+//которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+System.Console.WriteLine("When you input two numbers, i'll show you which is more, which is less. \nEnter first number:");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter second number:");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    System.Console.WriteLine($"{firstNumber} is bigger than {secondNumber}");
+}
+if(secondNumber > firstNumber)
+{
+    System.Console.WriteLine($"{firstNumber} is less than {secondNumber}");
+}
+if(secondNumber == firstNumber)
+{
+    System.Console.WriteLine($"{firstNumber} is equal to {secondNumber}");
+}
